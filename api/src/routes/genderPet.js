bf3a21058818f5498a6480a905b200cf0b1b9440
DaviFreitas77 <146572313+DaviFreitas77@users.noreply.github.com>
2025-03-28 22:59:36 -0300

    add

--- api/src/routes/genderPet.js
+++
@@ -1,8 +0,0 @@
-const genderController = require('../controllers/genderPetController');
-const express = require('express');
-const routes = express.Router();
-
-routes.post('/genderPet',genderController.store);
-
-
-module.exports = routes;
